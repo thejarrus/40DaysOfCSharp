a557774a98766a51a938713588c283f65a20f4b8
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-08-04 19:24:36 +0300

    Add files via upload

---
+++ Day10_Tenary/Day10_Tenary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 7. Kullanıcıdan giriş alarak yıldız bastırma
+        Console.Write("Kaç yıldız bastırmak istersin?: ");
+        string giris = Console.ReadLine();
+        if (int.TryParse(giris, out int yildizSayisi))
+
+        {
+            for (int i = 0; i < yildizSayisi; i++)
+
+            {
+                Console.WriteLine("*");
+            }
+            Console.WriteLine();
+
+        }
+
+        else
+        {
+            Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+        }
+    }
+}
