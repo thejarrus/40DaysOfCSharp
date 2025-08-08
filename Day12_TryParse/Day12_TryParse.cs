c9138775360a961f54a82d9318079eb96c39375b
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-08-08 18:26:42 +0300

    Add files via upload

---
+++ Day12_TryParse/Day12_TryParse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Güvenli veri alma örneği
+        Console.WriteLine("Lütfen bir sayı giriniz: ");
+        string girilenDeger = Console.ReadLine();
+
+        if (int.TryParse(girilenDeger, out int yas))
+        {
+            //Dönüşüm Başarılıysa
+            Console.WriteLine($"Yaşınız: {yas}");
+        }
+
+        else
+        {
+            //Dönüşüm Başarısızsa
+            Console.WriteLine("Hatalı giriş!");
+        }
+        
+    }
+}
+ 
