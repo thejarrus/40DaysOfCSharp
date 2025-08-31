b855265e639cd589c1600459e52b300b551c80c4
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-08-31 13:02:40 +0300

    Add files via upload

---
+++ Day18_StringEqualsEmptyString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string bos1 = "";
+        string bos2 = String.Empty;
+
+        Console.WriteLine(bos1 == bos2); // True
+
+        string a = "Merhaba";
+        string b = "merhaba";
+
+        // == operatörü
+        Console.WriteLine(a == b); // False
+
+        // String.Equals
+        Console.WriteLine(a.Equals(b)); // False
+    Console.WriteLine(a.Equals(b, StringComparison.OrdinalIgnoreCase)); // True
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
