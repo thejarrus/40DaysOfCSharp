72ca96884adf797d02285e09d4d3ef5fdbd48b45
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-09-20 10:09:10 +0300

    Add files via upload

---
+++ Day19_String Concatenation/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program {
+
+    static void Main(string[] args)
+    {
+        string ad = "Ali";
+        string soyad = "Yılmaz";
+
+        string tamIsim = ad + " " + soyad;
+
+        Console.WriteLine(tamIsim);
+
+        string mesaj = "Hoş geldin " + ad + "! Bugün günlerden " + DateTime.Now.DayOfWeek;
+        Console.WriteLine(mesaj);
+
+    }
+}
