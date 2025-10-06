2b71150b20a1ce8577d5bca0fbf7780f38994cc9
Mehmet Yiğit Kasap <71186488+thejarrus@users.noreply.github.com>
2025-10-06 10:10:20 +0300

    Add files via upload

---
+++ Day22_Exercises/sifre denetleme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your message: ");
+        string message = Console.ReadLine() ?? "";
+
+        for (int i = message.Length - 1; i >= 0; i--)
+        {
+            Console.Write(message[i]);
+        }
+        Console.WriteLine();
+    }
+}
